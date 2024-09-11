7ebe8dcf4d4b36f94ffea420608ee0cf9235806d
Santosh_Chappidi <santoshchappidi03@gmail.com>
2024-09-11 12:18:20 +0530

    updates

--- app/components/HomePage/HomePage.tsx
+++ app/components/HomePage/HomePage.tsx
@@ -17,17 +17,19 @@ type Props = {
 
 const HomePage = ({ photos }: Props) => {
   return (
-    <div className="h-auto w-full border border-red-700 ">
+    <div className="h-auto w-full border border-red-700 2xl:my-5 2xl:px-5 ">
       <div className="h-auto w-full border border-green-600 grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5 place-items-center">
         {photos?.map((item) => (
-          <div className="h-auto w-full border border-black" key={item.id}>
-            <Image
-              src={item.urls.small}
-              alt={item.alt_description || "gallery"}
-              className="object-contain"
-              width={500}
-              height={100}
-            />
+          <div key={item.id}>
+            <div className="h-auto w-full overflow-hidden rounded-sm">
+              <Image
+                src={item.urls.small}
+                alt={item.alt_description || "gallery"}
+                className="object-contain hover:scale-125 transition-all"
+                width={500}
+                height={100}
+              />
+            </div>
             <h2>{item.title}</h2>
             <p>{item.alt_description}</p>
           </div>
