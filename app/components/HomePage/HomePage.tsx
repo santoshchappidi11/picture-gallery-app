47dd9348d7675df39a0fcbf939fd6e85c5af2eb3
Santosh_Chappidi <santoshchappidi03@gmail.com>
2024-09-11 21:51:33 +0530

    updates

--- app/components/HomePage/HomePage.tsx
+++ app/components/HomePage/HomePage.tsx
@@ -12,15 +12,15 @@ const HomePage: React.FC = () => {
     <div className="h-auto w-full 2xl:mt-10 2xl:mb-5 2xl:px-5">
       <div className="h-auto w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 place-items-center ">
         {photos?.map((item) => (
-          <div key={item.id} className="h-auto">
-            <div className="relative h-96 w-full overflow-hidden rounded-sm">
+          <div key={item.id} className="h-full w-full border">
+            <div className="relative h-96 w-full overflow-hidden rounded-sm border">
               <Image
                 src={item.urls.small}
                 alt={item.alt_description || "gallery"}
                 // width={400}
                 // height={400}
                 layout="fill" // Use 'fill' to cover the parent container
-                className="w-full h-full object-cover transition-transform duration-300 ease-in-out transform hover:scale-110"
+                className="object-cover transition-transform duration-300 ease-in-out transform hover:scale-110"
               />
             </div>
             <h2 className="">{item.title}</h2>
